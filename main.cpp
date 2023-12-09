d78ff22ca83e7026924eedbbb06d1e3c2cc13b51
PangBizhang's PC <3096479972@qq.com>
2023-12-10 00:31:54 +0800

    feat: ✨双串口透传demo

--- main.cpp
+++ main.cpp
@@ -5,13 +5,49 @@
 #include "CSerialPort/SerialPort.h"
 #include "CSerialPort/SerialPortInfo.h"
 
-static int connect_port(void)
-{
-    return 0;
-}
+
+static CSerialPort* port1 = NULL;
+static CSerialPort* port2 = NULL;
 
 int main(void)
 {
     auto portList = CSerialPortInfo::availablePortInfos();
+    for( auto it = portList.begin() ; it != portList.end() ; ++it )
+    {
+        std::cout<<"port name : "<<it->portName<<std::endl;
+        std::cout<<"port desc : "<<it->description<<std::endl;
+    }
+
+    port1 = new CSerialPort;
+    port2 = new CSerialPort;
+
+    port1->init( "COM1" , 115200 );
+    port1->setOperateMode(AsynchronousOperate);
+    port1->setReadBufferSize(512);
+    port1->open();
+
+    port2->init( "COM3" , 115200 );
+    port2->setOperateMode(AsynchronousOperate);
+    port2->setReadBufferSize(512);
+    port2->open();
+
+    while(1)
+    {
+        int cnt;
+        uint8_t buf[512];
+
+        cnt = port1->readAllData( buf );
+        if( cnt > 0 )
+        {
+            port2->writeData(buf,cnt);
+        }
+
+        cnt = port2->readAllData( buf );
+        if( cnt > 0 )
+        {
+            port1->writeData(buf,cnt);
+        }
+    }
+
     return 0;
 }
